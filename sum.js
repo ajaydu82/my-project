59165e136215e11c1047d130c0b0b7d8156b15ff
ajay <ajaydu82@gmail.com>
2023-03-14 01:37:03 +0530

    first commit

---
+++ sum.js
@@ -0,0 +1,11 @@
+let sum=0;
+for(let i=0; i<10; i++){
+sum+=i;
+
+}
+console.log(sum)
+console.log(typeof null)
+console.log(null==undefined)
+console.log(null===undefined)
+var a=1;
+console.log(undefined===a)
